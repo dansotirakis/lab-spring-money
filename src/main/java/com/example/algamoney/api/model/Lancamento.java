4e20a23f0e3da968cbc8ce827c00bcfacd8984ce
Damianos Sotirakis <dansotirakis18@gmail.com>
2019-09-13 17:56:37 -0300

    Implementando BeanValidate nos atributos do lançamento

--- src/main/java/com/example/algamoney/api/model/Lancamento.java
+++ src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /* 
  *	@author Damianos Sotirakis
@@ -28,6 +29,8 @@ public class Lancamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
 	private String descricao;
 	
 	@Column(name = "data_vencimento")
@@ -36,17 +39,21 @@ public class Lancamento {
 	@Column(name = "data_pagamento")
 	private LocalDate dataPagamento;
 	
+	@NotNull
 	private BigDecimal valor;
 	
 	private String observacao;
 	
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
