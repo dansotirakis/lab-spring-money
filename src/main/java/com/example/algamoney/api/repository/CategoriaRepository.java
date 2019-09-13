c26455cd5fb0c6bc0c81ed069f150462a69936c8
Damianos Sotirakis <dansotirakis18@gmail.com>
2019-09-13 14:40:19 -0300

    Comments - Repository

--- src/main/java/com/example/algamoney/api/repository/CategoriaRepository.java
+++ src/main/java/com/example/algamoney/api/repository/CategoriaRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Categoria;
 
+/* 
+ *	@author Damianos Sotirakis
+ * 
+ * 	Interface criada para implementação dos métodos de persistencia rest do objeto categoria
+ * 
+ * */
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 }
--- src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Pessoa;
 
+/* 
+ *	@author Damianos Sotirakis
+ * 
+ * 	Interface criada para implementação dos métodos de persistencia rest do objeto pessoa
+ * 
+ * */
+
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
 }
